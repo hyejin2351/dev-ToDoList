198a54f8f69286b492b68f318a7d127e9741671b
hyejin Kim <hyejin2351@gmail.com>
2018-11-22 15:28:55 +0900

    일정관리 컴포넌트 생성

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PageTemplate from "./PageTemplate";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                일정관리
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <PageTemplate>안녕하세요</PageTemplate>
+      </div>
+    );
+  }
 }
 
 export default App;
